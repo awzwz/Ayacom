639b9dd5b3a541d2360530061c2424854db8269f
awzwz <awzwz0401@gmail.com>
2026-04-04 17:01:30 +0500

    CORS: allow all origins for hackathon deploy

--- is_uto/app/main.py
+++ is_uto/app/main.py
@@ -49,8 +49,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://frontend:3000",
-                   os.getenv("FRONTEND_URL", "")],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
